0f127cc1c88a4a3358372b42ce7d7b0089bbc1b6
Victor <cool.badmaev@bk.ru>
2023-04-22 20:44:26 +0300

    added second function

--- str_func.py
+++ str_func.py
@@ -5,3 +5,10 @@ def upper_word(word):
 print("Введите слово которое нужно увеличить")
 s = input()
 print(upper_word(s))
+"""  Function checked """
+def title_word(phrase):
+    return phrase.title()
+
+print("Напишите фразу для увеличения первой буквы")
+e = input()
+print(title_word(e))
